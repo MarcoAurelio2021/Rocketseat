3d8c3feaf4d14096c9e19821a4c1c0d5ef2dd638
Marco Aurelio de AVILA <marcoaureliodavila_dev@outlook.com>
2021-07-21 22:32:29 -0300

    estudos_js

--- exercicios_js/script.js
+++ exercicios_js/script.js
@@ -2,8 +2,8 @@
 
 crie um algoritmo que transforme as notas do sistema numérico para sistema de notas em caracteres A, B, C 
 */
-
-let note = 64
+/*
+let note = 0
 
 if (note >= 90) {
   console.log('Nota A')
@@ -13,6 +13,39 @@ if (note >= 90) {
   console.log('Nota C')
 } else if (note < 70 && note >= 60) {
   console.log('Nota D')
-} else {
+} else if (note < 60 && note >= 0) {
   console.log('Nota F')
+} else {
+  console.log('Nota inválida')
+}
+/*
+
+
+/* ======================================================== */
+
+// Transforma notas escolares
+
+// crie um algoritmo que transforme as notas do sistema numérico para sistema de notas em caracteres A, B, C
+
+let score = 94
+let scoreA = score >= 90 && score < 100
+let scoreB = score >= 80 && score <= 89
+let scoreC = score >= 70 && score <= 79
+let scoreD = score >= 60 && score <= 69
+let scoreF = score < 60 && score >= 0
+
+let scoreFinal = 'Nota inválida'
+
+if (score >= 90 && score <= 100) {
+  console.log('A')
+} else if (score >= 80 && score <= 89) {
+  console.log('B')
+} else if (score >= 70 && score <= 79) {
+  console.log('C')
+} else if (score >= 60 && score <= 69) {
+  console.log('D')
+} else if (score < 60 && score >= 0) {
+  console.log('F')
+} else {
+  console.log('Nota inválida')
 }
