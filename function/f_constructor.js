b8c844026e56dd0d51615f4ae01293ebbab5bbc1
Marco Aurelio Avila <marcoaurelio_dev@outlook.com>
2022-04-05 21:36:52 -0300

    F_Construtora

--- function/f_constructor.js
+++ function/f_constructor.js
@@ -6,10 +6,21 @@
 
 */
 
-function person(name) {
-  this.name = name
-}
-const MarcoAurelio = new person('Marco')
-const Maria = new person('Morena')
-console.log(MarcoAurelio)
-console.log(Maria)
+function Person (name) {
+  // this (vai referenciar o "marco" que está fora do escopo)
+ // e o "marco" que está fora do escopo vai ser referenciado como this.
+ this // "marco"
+ .name // .name é uma propriedade que vai receber o (parametro)
+ = name // o "name" é o parametro, que recebe o argumento que está dentro da new Person() 
+ 
+ }
+ 
+ const marco = new Person("Marco Aurélio") // essa função construtora vai retorna para a /*const marco*/ um novo objeto. 
+ 
+ const Maria = new Person("Maria Morena")
+ 
+ console.log(marco)
+ console.log(Maria);
+ 
+ 
+ 
