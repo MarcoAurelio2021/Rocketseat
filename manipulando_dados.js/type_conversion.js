92cd03daeac8dbdd5bfa29f58a3679e973df0d43
Marco Aurelio Avila <marcoaurelio_dev@outlook.com>
2022-04-06 21:17:25 -0300

    type_conversion

---
+++ manipulando_dados.js/type_conversion.js
@@ -0,0 +1,7 @@
+/* Type conversion (Typecasting) vs Type coersion
+
+Alteração de um tipo de dado para outro tipo */
+
+console.log("9" + 5 /* o java script vai Number 5 em uma string e concatenar os valores. */)
+
+// o operador "+" serve tanto para soma e para concatenar valores.
