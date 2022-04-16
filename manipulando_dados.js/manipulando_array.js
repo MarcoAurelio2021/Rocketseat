791b85b680c93c88a2ac25a5295915d85ab4e53c
marcoaurelio <marcoaurelio_dev@outlook.com>
2022-04-15 22:48:52 -0300

    manipulando_elementos_dentro_de_um_array

--- manipulando_dados.js/manipulando_array.js
+++ manipulando_dados.js/manipulando_array.js
@@ -4,15 +4,22 @@ let techs = ['html', 'css', 'js']
 
 // adicionar um item
 techs.push('nodejs')
+
 // adicionar no começo
 techs.unshift('sql')
-// remover do fim
-techs.pop()
-// remover do começo
-// pegar somente alguns elementos da array
-// remover 1 ou mais itens em qualquer posição da array
-// encontrar a posição de um elemento na array
-console.log(techs)
 
-// teste envio para git hub
-// troquei meu email no terminal
+// remover do fim
+//techs.pop()
+
+// remover do começo
+// techs.shift()
+
+// pegar somente alguns elementos da array
+console.log(techs.slice(1,3)) // fiquei na dúvida
+
+// remover 1 ou mais itens em qualquer posição da array
+techs.splice() //??
+
+// encontrar a posição de um elemento na array
+let index = techs.indexOf("nodejs")
+
--- manipulando_dados.js/script.js
+++ manipulando_dados.js/script.js
@@ -1,1 +1,23 @@
-  
+let techs = ['html', 'css', 'js']
+
+// adicionar um item
+techs.push('nodejs')
+
+// adicionar no começo
+techs.unshift('sql')
+
+// remover do fim
+//techs.pop()
+
+// remover do começo
+// techs.shift()
+
+// pegar somente alguns elementos da array
+console.log(techs.slice(1,3)) // fiquei na dúvida
+
+// remover 1 ou mais itens em qualquer posição da array
+techs.splice() //??
+
+// encontrar a posição de um elemento na array
+let index = techs.indexOf("nodejs")
+techs.splice(index, 1)
